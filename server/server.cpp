dfdc46935b7eda47afbaea475267ee5e98a133e6
s257117 <s257117@studenti.polito.it>
2019-10-05 00:43:55 +0200

    server folder added

---
+++ server/server.cpp
@@ -0,0 +1,70 @@
+#include "server.h"
+
+static QString getIdentifier(QWebSocket *peer)
+{
+    return QStringLiteral("%1:%2").arg(peer->peerAddress().toString(),
+                                       QString::number(peer->peerPort()));
+}
+
+//! [constructor]
+Server::Server(quint16 port, QObject *parent) :
+    QObject(parent),
+    m_pWebSocketServer(new QWebSocketServer(QStringLiteral("Chat Server"),
+                                            QWebSocketServer::NonSecureMode,
+                                            this))
+{
+    if (m_pWebSocketServer->listen(QHostAddress::Any, port))
+    {
+        QTextStream(stdout) << "Chat Server listening on port " << port << '\n';
+        connect(m_pWebSocketServer, &QWebSocketServer::newConnection,
+                this, &Server::onNewConnection);
+    } else {
+        qDebug() << m_pWebSocketServer->errorString();
+    }
+}
+
+Server::~Server()
+{
+    m_pWebSocketServer->close();
+}
+//! [constructor]
+
+//! [onNewConnection]
+void Server::onNewConnection()
+{
+    auto pSocket = m_pWebSocketServer->nextPendingConnection();
+    QTextStream(stdout) << getIdentifier(pSocket) << " connected!\n";
+    pSocket->setParent(this);
+
+    connect(pSocket, &QWebSocket::textMessageReceived,
+            this, &Server::processMessage);
+    connect(pSocket, &QWebSocket::disconnected,
+            this, &Server::socketDisconnected);
+
+    m_clients << pSocket;
+}
+//! [onNewConnection]
+
+//! [processMessage]
+void Server::processMessage(const QString &message)
+{
+    QWebSocket *pSender = qobject_cast<QWebSocket *>(sender());
+    for (QWebSocket *pClient : qAsConst(m_clients)) {
+        if (pClient != pSender) //don't echo message back to sender
+            pClient->sendTextMessage(message);
+    }
+}
+//! [processMessage]
+
+//! [socketDisconnected]
+void Server::socketDisconnected()
+{
+    QWebSocket *pClient = qobject_cast<QWebSocket *>(sender());
+    QTextStream(stdout) << getIdentifier(pClient) << " disconnected!\n";
+    if (pClient)
+    {
+        m_clients.removeAll(pClient);
+        pClient->deleteLater();
+    }
+}
+//! [socketDisconnected]
