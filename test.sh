40efe839ea3a92fe052a18502570d01820a52366
DavideAntoninoGiorgio94 <davideantonino94@gmail.com>
2019-10-09 18:44:51 +0200

    cleaning branch

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This small script opens three tabs and executes
-# the server as well as two instances of the client
-# N.B. Please take care that you build the applications in the
-# right folders (../build-PoliDoxXXX/)
-gnome-terminal --tab -e ../build-PoliDoxServer/polidox-server
-gnome-terminal --tab -e ../build-PoliDoxClient/polidox-client
-gnome-terminal --tab -e ../build-PoliDoxClient/polidox-client
