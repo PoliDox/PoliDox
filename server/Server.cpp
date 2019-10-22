f2132a17d7b2136a4109169599643111441bcc00
FedericoVasile1 <federicovasile1@gmail.com>
2019-10-22 18:19:41 +0200

    fixed return value on db functions

--- server/Server.cpp
+++ server/Server.cpp
@@ -162,7 +162,7 @@ void Server::handleLoggedRequests(const QString &genericRequestString){
     }
     else if (header == "createFileReq"){
         //create and open the file
-        double result = this->dbOperations->insertNewDocument(nameDocument);
+        bool result = this->dbOperations->insertNewDocument(nameDocument);
 
         if(result){
             QList<QString> emptyList;
