cc191b0f63bbea16f0d63ca55b627d988ba7d9a8
DavideAntoninoGiorgio94 <davideantonino94@gmail.com>
2019-11-25 22:09:36 +0100

    Update change image

--- client/Profile.cpp
+++ client/Profile.cpp
@@ -1,4 +1,7 @@
 #include <QIcon>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <qdebug.h>
 #include "Profile.h"
 #include "ui_profile.h"
 
@@ -7,7 +10,6 @@ Profile::Profile(QWidget *parent) :
     ui(new Ui::Profile)
 {
     ui->setupUi(this);
-    this->ChangeImgDialog = new ChangeImg(this);
     this->ChangePwdDialog = new ChangePwd(this);
 }
 
@@ -31,5 +33,28 @@ void Profile::on_changePassword_clicked()
 
 void Profile::on_changeImage_clicked()
 {
-    this->ChangeImgDialog->show();
+    //qui fare l'upload dell'immagine
+    QFileDialog file_selection(ui->image_pic);
+    file_selection.setNameFilter(tr("JPEG (*.jpg *.jpeg *.png)"));
+
+    file_selection.show();
+
+    QString filePath;
+    if (file_selection.exec())
+         filePath = file_selection.selectedFiles().at(0);
+
+    QPixmap pix(filePath);
+    QFile img(filePath);
+
+    if(img.size()>10000000){
+        QMessageBox::warning(this, "ImgWarning", "The file's dimension is greater than 10MB!");
+        return;
+    }
+
+    if(filePath.size()>0){
+        ui->image_pic->setPixmap(pix.scaled(200, 200));
+        QMessageBox::information(this, "PoliDox", "Image correctly updated");
+    }
+
+    //TODO:inviare il messaggio al server con la nuova immagine
 }
