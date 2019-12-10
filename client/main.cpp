ec1391bb489efd885a083778193cf05dde401321
DavideAntoninoGiorgio94 <davideantonino94@gmail.com>
2019-12-10 19:40:45 +0100

    Added main try catch

--- client/main.cpp
+++ client/main.cpp
@@ -7,16 +7,24 @@
 
 int main(int argc, char *argv[])
 {
-    QString ip_address("127.0.0.1");
-    if (argc == 2){
-        ip_address = QString(argv[1]);
+    try {
+
+        QString ip_address("127.0.0.1");
+        if (argc == 2){
+            ip_address = QString(argv[1]);
+        }
+        QApplication app(argc, argv);
+        QCoreApplication::setOrganizationName("PoliDox");
+        QCoreApplication::setApplicationName("PoliDox");
+        QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+
+        Client l_client(ip_address);
+
+        app.exec();
+
+    } catch (...) {
+        return EXIT_FAILURE;
     }
-    QApplication app(argc, argv);
-    QCoreApplication::setOrganizationName("PoliDox");
-    QCoreApplication::setApplicationName("PoliDox");
-    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
 
-    Client l_client(ip_address);
-
-    return app.exec();
+    return EXIT_SUCCESS;
 }
